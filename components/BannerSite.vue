c10f1af2cffdcea1391ee5ca1d8beb5f8f08ccdc
Vinicius de Araujo Barboza <dev.viniciusbarboza@gmail.com>
2024-06-29 20:27:49 -0300

    Banner Site

---
+++ components/BannerSite.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <section class="hero is-small hero-background">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <img class="title has-text-white responsive-image animate__animated animate__pulse" src="~/assets/images/logo.png" />
+                    <p class="subtitle has-text-dark is-5 has-text-weight-semibold mt-3">Forros - Molduras - Gesso
+                        Acartonado</p>
+                    <p class="subtitle has-text-dark is-5 has-text-weight-semibold margin-top">Divisorias - Sancas -
+                        Colunas - Arte em Gesso</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BannerSite'
+}
+</script>
+
+<style>
+.hero-background {
+    background-image: url('~/assets/images/bannerMeio.png');
+    background-size: cover;
+    background-position: center;
+}
+
+.responsive-image {
+    height: 10vw;
+    /* Ajuste o valor conforme necessário */
+}
+
+@media (max-width: 768px) {
+
+    /* Para dispositivos móveis */
+    .responsive-image {
+        height: 20vw;
+        /* Ajuste o valor conforme necessário */
+    }
+}
+
+.subtitle {
+    font-size: 2vw;
+    /* Ajuste o valor conforme necessário */
+}
+
+@media (max-width: 768px) {
+
+    /* Para dispositivos móveis */
+    .subtitle {
+        font-size: 3vw !important;
+        /* Ajuste o valor conforme necessário */
+    }
+}
+
+.margin-top {
+    margin-top: -25px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <NavBar />
+    <BannerSite />
     <Nuxt />
   </div>
 </template>
 
 <script>
 import NavBar from '~/components/NavBar.vue'
+import BannerSite from '~/components/BannerSite.vue'
+
 export default {
   name: 'DefaultLayout',
   components: {
     NavBar,
+    BannerSite,
   }
 
 }
