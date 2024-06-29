609dcf087ecec3db584a08dd0db5afcc554adce8
Vinicius de Araujo Barboza <dev.viniciusbarboza@gmail.com>
2024-06-29 20:52:29 -0300

    Adicionado Cards

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,21 @@
     <NavBar />
     <BannerSite />
     <Nuxt />
+    <FooterSite />
   </div>
 </template>
 
 <script>
 import NavBar from '~/components/NavBar.vue'
 import BannerSite from '~/components/BannerSite.vue'
+import FooterSite from '~/components/FooterSite.vue';
 
 export default {
   name: 'DefaultLayout',
   components: {
     NavBar,
     BannerSite,
+    FooterSite
   }
 
 }
