031dca4a1a834dbce1f62c2d3b20a609ecc52f54
Vinicius de Araujo Barboza <dev.viniciusbarboza@gmail.com>
2024-06-29 20:41:18 -0300

    Teste Cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
 
-    <div class="container mt-5">
+    <div class="container mt-5 is-max-widescreen">
       <section class="bordered has-text-centered">
         <p class="subtitle is-6 has-text-weight-semibold m-5">NOSSOS PRODUTOS</p>
       </section>
@@ -13,18 +13,56 @@
 
     <div class="container">
 
-      <div class="card">
-        <div class="card-image">
-          <figure class="image">
-            <img src="~/assets/images/inicio/01.jpg" alt="Forro Liso">
-          </figure>
+      <div class="grid is-col-min-1">
+        <div class="cell">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img src="~/assets/images/inicio/01.jpg" alt="Forro Liso">
+              </figure>
+            </div>
+            <div class="card-content">
+              <h2 class="title is-4">Forro Liso</h2>
+              <p class="is-size-7">É a tendência do momento usado para revestir paredes e teto pela sua versatilidade,
+                plasticidade e
+                acabamento. Ele traz um toque sutil e fino, paredes com bom acabamento de Gesso Liso e uma boa tinta é
+                notória sua diferença e classe.</p>
+            </div>
+          </div>
         </div>
-        <div class="card-content">
-          <h2 class="title is-4">Forro Liso</h2>
-          <p class="is-size-7">É a tendência do momento usado para revestir paredes e teto pela sua versatilidade,
-            plasticidade e
-            acabamento. Ele traz um toque sutil e fino, paredes com bom acabamento de Gesso Liso e uma boa tinta é
-            notória sua diferença e classe.</p>
+
+        <div class="cell">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img src="~/assets/images/inicio/01.jpg" alt="Forro Liso">
+              </figure>
+            </div>
+            <div class="card-content">
+              <h2 class="title is-4">Forro Liso</h2>
+              <p class="is-size-7">É a tendência do momento usado para revestir paredes e teto pela sua versatilidade,
+                plasticidade e
+                acabamento. Ele traz um toque sutil e fino, paredes com bom acabamento de Gesso Liso e uma boa tinta é
+                notória sua diferença e classe.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="cell">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image">
+                <img src="~/assets/images/inicio/01.jpg" alt="Forro Liso">
+              </figure>
+            </div>
+            <div class="card-content">
+              <h2 class="title is-4">Forro Liso</h2>
+              <p class="is-size-7">É a tendência do momento usado para revestir paredes e teto pela sua versatilidade,
+                plasticidade e
+                acabamento. Ele traz um toque sutil e fino, paredes com bom acabamento de Gesso Liso e uma boa tinta é
+                notória sua diferença e classe.</p>
+            </div>
+          </div>
         </div>
       </div>
 
